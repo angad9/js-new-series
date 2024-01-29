797f35a2cf6e6c73500ae712d3c9cce5ce440124
angad9 <73772102+angad9@users.noreply.github.com>
2024-01-29 18:55:47 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all JS code as newer version
+
+let name = "Angad"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// bbolean => true/false
+// null => standlone value
+// udefined => 
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
