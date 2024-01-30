345e45e05360767f5010f0900cf0de6b91bb14c9
angad9 <73772102+angad9@users.noreply.github.com>
2024-01-30 18:15:15 +0000

    All about Dates

---
+++ 01_basic/07_datesInJs.js
@@ -0,0 +1,10 @@
+// Dates 
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 0, 23)
+console.log(myCreatedDate.toDateString());
