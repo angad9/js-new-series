77cd0d5f4e91a348f1cafbb62ed8572517fac39c
angad9 <73772102+angad9@users.noreply.github.com>
2024-01-30 14:36:17 +0000

    conversion and comparision

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
  let score = "33" 
 
- console.log(typeof score);
- console.log(typeof(score));
+//  console.log(typeof score);
+//  console.log(typeof(score));
 
  let valueInNumber = Number(score)
- console.log(typeof valueInNumber);
- console.log(valueInNumber);
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
 
 
  // "33" => 33
@@ -15,7 +15,7 @@
  let isLoggedIn = 1
 
  let booleanIsLoggedIn = Boolean(isLoggedIn )
- console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
  // 1 => true;  0 => false 
  // "" => false 
@@ -24,5 +24,16 @@
  let someNumber = 33 
 
  let stringNumber = String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+
+ // ************** Operations *******************
+
+ let value = 3
+ let negValue = -value
+ console.log(negValue);
+
+
+
+
