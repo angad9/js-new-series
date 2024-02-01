3e6007095abf4ec61a5fb56801bc095fab175902
angad9 <73772102+angad9@users.noreply.github.com>
2024-02-01 08:52:42 +0000

    Object Related 2

---
+++ 02_basic/04_objects2.js
@@ -0,0 +1,58 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+  tinderUser.id = "123abc"
+  tinderUser.name = "Sammy"
+  tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullname: {
+            firstname: "angad",
+            lastname: "magar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 2: "b"}
+const obj4 = {4: "a", 6: "b"}
+
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "magarangad@gmail.com"
+    },
+    {
+        id: 1,
+        email: "magarangad@gmail.com"
+    },
+    {
+        id: 1,
+        email: "magarangad@gmail.com"
+    },
+    {
+        id: 1,
+        email: "magarangad@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
