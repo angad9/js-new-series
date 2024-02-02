80330e75d5076ef823b8c87a65d65c342bb0546e
angad9 <73772102+angad9@users.noreply.github.com>
2024-02-02 15:33:22 +0000

    Scope Final

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -4,11 +4,59 @@ if (true) {
     let a = 10 
     const b = 20  
     console.log("INNER:", a);
-    
 }
 
 
 
-console.log(a);
+
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// Nested Scope 
+
+function one (){
+    const username = "hitesh"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+//one()
+
+
+if (true) {
+    const username = "angad"
+    if (username === "angad") {
+        const website = "youtube"
+        console.log(username + website);
+
+    }
+   // console.log(website);
+}
+
+// console.log(username);
+
+
+// ****************** interesting ***********
+
+
+console.log(addone(5))
+function addone (num){
+    return num +1
+}
+
+
+addTwo(5)
+const addTwo = function (num) {
+    return num + 2
+}
+
+
