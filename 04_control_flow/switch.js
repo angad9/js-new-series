0dfd5633805d98818c065d08ed7c9215c04a6ee8
Angad Magar <73772102+angad9@users.noreply.github.com>
2024-02-14 19:11:14 +0000

    Switches in javascript

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,55 @@
+// switch (key) {
+//     case value:
+  
+//         break;
+
+//     default:
+//         break;    
+
+// }
+
+// int in no or no in input
+
+const monthd = 3
+
+switch (monthd) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// String in input
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
