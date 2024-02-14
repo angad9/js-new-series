dce37f102a8e888d2b40baeeb18a6f186fd0eca2
Angad Magar <73772102+angad9@users.noreply.github.com>
2024-02-14 18:50:37 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+// if
+const isUserloggedIn = true 
+const temperature = 42 
+
+if (temperature < 50 ) {
+    console.log("less than 50 ");
+
+} else {
+    console.log("temperature is greature than 50");
+
+}
+
+console.log("Execute");
+
+const score = 200 
+
+if (score > 100) {
+    let power = "fly"
+    console.log(`user power: ${power}`);
+}
+
+console.log(`user power : ${power}`);
+
+const balabnce = 1000
+
+// if (balabnce > 500) console.log("test"), console.log("test2");
+
+if (balabnce < 500 ) {
+    console.log("less than");
+
+} else if (balabnce < 750) {
+    console.log("less than 750");
+
+} else {
+    console.log("less than 1200");
+}
+
+const userLoggdIn = true 
+const debitCard = true 
+const loggedInFromGoogle = false 
+const loggedInFromEmail = true 
+
+if (userLoggdIn && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+
+}
