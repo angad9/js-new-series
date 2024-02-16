7f04b89b757e8129548a84ea4b622ba521e70eeb
Angad Magar <73772102+angad9@users.noreply.github.com>
2024-02-16 15:19:38 +0000

    forif loop

--- 05_iteration/four.js
+++ 05_iteration/four.js
@@ -9,3 +9,20 @@ for (const key of myObject) {
     console.log(`${key} shortcut is for ${myObject[key]}`);
     
 }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in object) {
+    console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States Of America ")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
