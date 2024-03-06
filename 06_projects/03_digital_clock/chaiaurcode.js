05f135cb9f36fc39ae29a01092fdbea5351e12ee
Angad Magar <73772102+angad9@users.noreply.github.com>
2024-03-06 06:21:06 +0000

    js file

---
+++ 06_projects/03_digital_clock/chaiaurcode.js
@@ -0,0 +1,7 @@
+document.getElementById('clock');
+// document.querySelector('#clock')
+
+setInterval (function () {
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
